e1769dd8049041b4471efeb4950a5b5725e6ad7c
mscalel <msksaul@gmail.com>
2022-09-01 16:07:01 -0600

    Chapter 1

---
+++ c1/index.js
@@ -0,0 +1,77 @@
+let dineroCofla = prompt('cuanto dinero tienes Cofla?')
+let dineroRoberto = prompt('cuanto dinero tienes Roberto?')
+let dineroPedro = prompt('cuanto dinero tienes Pedro?')
+
+dineroCofla = parseInt(dineroCofla)
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+  alert('comrate el helado de agua')
+  alert('y te sobran ' + (dineroCofla - 0-6))
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+  alert('comprate el helado de crema')
+  alert('y te sobran ' + (dineroCofla - 1))
+}
+else  if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+  alert('comprate el helado de healix')
+  alert('y te sobran ' + (dineroCofla - 1.6))
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+  alert('comprate el helado de heladovich')
+  alert('y te sobran ' + (dineroCofla - 1.7))
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+  alert('comprate el helado de healrdo')
+  alert('y te sobran ' + (dineroCofla - 1.8))
+}
+else if (dineroCofla >= 2.9) {
+  alert('comprate el helado de confites o el pode de 1/4 kg')
+  alert('y te sobran ' + (dineroCofla - 2.9))
+}
+else {
+  alert('lo siento no te alcanza para comprar ningun helado')
+}
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+  alert('comrate el helado de agua')
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+  alert('comprate el helado de crema')
+}
+else  if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+  alert('comprate el helado de healix')
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+  alert('comprate el helado de heladovich')
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+  alert('comprate el helado de healrdo')
+}
+else if (dineroRoberto >= 2.9) {
+  alert('comprate el helado de confites o el pode de 1/4 kg')
+}
+else {
+  alert('lo siento no te alcanza para comprar ningun helado')
+}
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+  alert('comrate el helado de agua')
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+  alert('comprate el helado de crema')
+}
+else  if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+  alert('comprate el helado de healix')
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+  alert('comprate el helado de heladovich')
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+  alert('comprate el helado de healrdo')
+}
+else if (dineroPedro >= 2.9) {
+  alert('comprate el helado de confites o el pode de 1/4 kg')
+}
+else {
+  alert('lo siento no te alcanza para comprar ningun helado')
+}
